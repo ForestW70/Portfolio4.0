90789846a6915912359b2e39128e761594eaf5ba
ForestW70 <hexaforest@gmail.com>
2021-07-22 16:41:50 -0400

    resume screen

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,7 +1,13 @@
+import { RiAlarmWarningLine } from 'react-icons/ri'
 
-
-export default function Resume(...props){
+export default function Resume(...props) {
     return (
-        <h2>resume</h2>
+        // <h2>resume</h2>
+        <div className="res-body">
+            <RiAlarmWarningLine className="res-svg"/>
+            <h2>Under construction!</h2>
+
+            <p><a href="https://forestw70.github.io/Portfolio3.0/resume.html">Check this out in the meantime!</a></p>
+        </div>
     )
 }
