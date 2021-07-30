8decd1f528df4281556e5632837c036f3ac829cd
ForestW70 <hexaforest@gmail.com>
2021-07-29 23:39:29 -0400

    thought about some esLint.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@
 export default function Footer(props) {
     return (
         <footer>
-            <span>© 1995-2021, Forest Wilson, Inc. or its affiliates. All rights reserved. *Sometimes known to not keep it simple. #brns-001</span>
+            <span>© 1995-2021, Forest Wilson, Inc. or its affiliates. All rights reserved. *Sometimes known to not keep it simple.</span>
         </footer>
     )
 }
