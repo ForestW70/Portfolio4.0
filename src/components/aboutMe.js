f2e60ba36a21e85753cd2b21e25e2c8f19fbe9dd
Forest wilson <hexaforest@gmail.com>
2022-02-17 18:58:40 -0500

    updated home view's about me. adjusted styles.

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -7,22 +7,37 @@ export default function AboutMe() {
 
                 <div className="about-p">
                     <p>
-                        I'm a web developer who recieved a certificate in full-stack development from the George Washington University. I'm thankful for my time there, becuase it opened up my eyes to the possibilities of programming and coding as a whole.
-
+                        <strong>Forest is a Full stack web developer</strong> foremost, and has developed over 20 applications utilizing the core web languages (HTML, CSS, and JavaScript) in conjunction with other technologies and packages such as:
+                        Node.js, React.js, Next.js, Bootstrap, MySql, MongoDB, Express, Jest, Handlebars, JQuery, Sequelize, Mongoose, IndexedDB, JWTs, Babel, Apollo, Axios, and GraphQL. He also has experience with NPM/NPX/Yarn, ESLint, Encryption, Cookies, Postman/Insomnia, and PWAs/Service workers.
                     </p>
 
                     <p>
-                        So far, I've developed over 20 websites and applications using Javascript, Node.js, and React, with the help of a long list of other technologies that I've learned along the way.
-                        
+                        He earned his Full-stack web development certificate from The George Washington University's 6 month coding bootcamp program, but behind the scenes, <strong>Forest likes to use his experience to develop music-oriented applications for the web,</strong> as well as creating personal, professional, and responsive websites for companies and indivuals.
+                        He is also very familiar with <strong>VSCode, Github/Git, and Microsoft/Adobe suite programs such as Excel, Photoshop, and Illustrator.</strong>
                     </p>
 
                     <p>
-                        Forest Wilson is a Full stack web developer foremost, and has developed over 20 applications utilizing the core web languages (HTML, CSS, and JavaScript) in conjunction with other technologies and packages such as:
-                        React.js, Next.js, Bootstrap, MySql, MongoDB, Express, Jest, Handlebars, JQuery, Sequelize, Mongoose, IndexedDB, JWTs, Babel, Apollo, Axios, and GraphQL.
-                        He also has experience with VSCode, Github, Git, Node, NPM/NPX/Yarn, Heroku, Encryption, Cookies, Postman/Insomnia, and Service workers.
+                        To add to the pot, he also has years of experience in fields such as Marketing/Economics, Service-sector/Communication, and IT Helpdesk/Office administration.
+                        The master plan is to one day wrap up all of these skills and create a vibrant online community of creators, where they can have a stylish and accessible web application to display, market, and sell their work.
                     </p>
 
                     <p>
+                        In the mean time, Forest is ready build-up his repertoire of front-end, back-end, and UX/UI skills to the max, and he's <strong>not afraid of the hard work</strong> it will take to achieve this. He is committed to designing creative and accessible applications, as well as developing a quality product with as few compromises as possible.
+                    </p>
+
+                    <div className="aboutDivider">
+                        <p>
+                            More about me and my projects: <a href="https://github.com/ForestW70">https://github.com/ForestW70</a>
+
+                        </p>
+
+                        <p>
+                            My current project: <a href="https://burnthis.space">https://burnthis.space</a>
+                        </p>
+                    </div>
+
+
+                    {/* <p>
                         Behind the scenes, Forest likes to find new ways to use his development experience to build music-oriented applications for the web, and for creating music in tandem with FL Studio and Garage band.
                         Forest is also very familiar with Microsoft and Adobe suite programs such as Excel, Photoshop, and Illustrator. To add to the pot, he also has years of experience in fields such as Marketing/Economics, Service sector/Communication, and IT Helpdesk/Office administration.
                         The master plan is to one day wrap up all of these skills and create a vibrant online community of creators, where they can have a stylish and accessible web application to display, market, and sell their work.
@@ -32,7 +47,7 @@ export default function AboutMe() {
                         In the mean time, Forest is ready build-up his repertoire of front-end, back-end, and UX/UI skills to the max, and he's not afraid of the hard work it will take to achieve this.
                         He is committed to designing creative and accessible applications, as well as developing a quality product with as few compromises as possible.
                         So if you are someone who also heavily believes in these ideas, and also in need of a web developer... Well you might just know who to message now.
-                    </p>
+                    </p> */}
 
 
                     <br></br>
